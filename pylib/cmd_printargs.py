5a150079fef00c4c52f16c7745d34add6e0ee710
liraz siri <liraz@turnkeylinux.org>
2010-02-06 05:39:53 +0200

    improved docs (README) + added help to cmd_printargs as an example

--- pylib/cmd_printargs.py
+++ pylib/cmd_printargs.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
-import sys
+"""Prints the cli arguments the user provided.
 
+This is not a very interesting command. You should see
+this description when you try asking for help on the command.
+"""
+import sys
+import help
+
+@help.usage(__doc__)
+def usage():
+    print >> sys.stderr, "Syntax: %s [args]" % sys.argv[0]
+    
 def main():
     print "printing args: " + `sys.argv`
     

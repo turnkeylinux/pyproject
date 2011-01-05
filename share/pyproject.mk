695c59379a25f41abb361185e58fd123594b9e69
Liraz Siri <liraz@turnkeylinux.org>
2011-01-05 23:25:14 +0200

    rewrote pyproject.mk to enable extension of main targets (Makefile inheritance)

--- share/pyproject.mk
+++ share/pyproject.mk
@@ -28,84 +28,9 @@ INSTALL_FILE_MOD = $(if $(INSTALL_SUID), 4755, 755)
 
 all: help
 
-help:
-	@echo '=== Configuration variables:'
-	@echo 'INSTALL_SUID   # if not empty string, install program suid'
-	@echo 'INSTALL_NODOC  # if not empty string, compile without docstrings'
-	@echo
-
-	@echo '=== Targets:'
-	@echo 'install   [ prefix=path/to/usr ] # default: prefix=$(value prefix)'
-	@echo 'uninstall [ prefix=path/to/usr ]'
-	@echo
-	@echo 'updatelinks                      # update toolkit wrapper links'
-	@echo
-	@echo 'rename name=<newname>'
-	@echo 'clean'
-	@echo
-	@echo 'dist                             # create distribution tarball'
-	@echo 'gitdist                          # create git distribution tarball'
-
 debug:
 	$(foreach v, $V, $(warning $v = $($v)))
 	@true
-
-rename:
-	$(if $(name),,($(error 'name' not set)))
-	$(PYPROJECT_SHARE_PATH)/rename.sh $(progname) $(name)
-
-updatelinks:
-	@echo -n updating links... " "
-	@$(PYPROJECT_SHARE_PATH)/updatelinks.sh
-	@echo done.
-	@echo
-
-execproxy: TRUEPATH_INSTALL = $(shell echo $(PATH_INSTALL) | sed -e 's/debian\/$(progname)//g')
-execproxy: execproxy.c
-	gcc execproxy.c -DMODULE_PATH=\"$(TRUEPATH_INSTALL)/wrapper.pyo\" -o _$(progname)
-	strip _$(progname)
-
-build: execproxy
-	$(PYCC) pylib/*.py *.py
-
-uninstall:
-	rm -rf $(PATH_INSTALL)
-	rm -rf $(PATH_INSTALL_SHARE)
-	rm -f $(PATH_BIN)/$(progname)
-
-	# delete links from PATH_BIN
-	for f in $(progname)-*; do rm -f $(PATH_BIN)/$$f; done
-
-install: build
-	@echo
-	@echo \*\* CONFIG: prefix = $(prefix) \*\*
-	@echo 
-
-	install -d $(PATH_BIN) $(PATH_INSTALL) $(PATH_INSTALL_LIB) $(PATH_INSTALL_LIBEXEC)
-
-	# if contrib exists
-	contrib=$$(echo contrib/*); \
-	if [ "$$contrib" != "contrib/*" ]; then \
-		mkdir -p $(PATH_INSTALL_CONTRIB); \
-		cp -a contrib/* $(PATH_INSTALL_CONTRIB); \
-	fi
-
-	install -m 644 pylib/*.pyo $(PATH_INSTALL_LIB)
-	-install -m 755 libexec/* $(PATH_INSTALL_LIBEXEC)
-
-	install -m 644 wrapper.pyo $(PATH_INSTALL)
-	python -O wrapper.py --version > $(PATH_INSTALL)/version.txt
-
-	for f in $(progname)*; do \
-		if [ -x $$f ]; then \
-			cp -P $$f $(PATH_BIN); \
-		fi; \
-	done
-	rm -f $(PATH_BIN)/$(progname)
-	install -m $(INSTALL_FILE_MOD) _$(progname) $(PATH_BIN)/$(progname)
-
-clean:
-	rm -f pylib/*.pyc pylib/*.pyo *.pyc *.pyo _$(progname)
 
 dist: clean
 	-mkdir -p $(PATH_DIST)
@@ -125,4 +50,104 @@ gitdist: clean
 	tar jcvf $(PATH_DIST)-git.tar.bz2 $(PATH_DIST)-git
 	rm -rf $(PATH_DIST)-git
 
-.PHONY: gitdist dist clean install uninstall build updatelinks rename debug help
+rename:
+	$(if $(name),,($(error 'name' not set)))
+	$(PYPROJECT_SHARE_PATH)/rename.sh $(progname) $(name)
+
+updatelinks:
+	@echo -n updating links... " "
+	@$(PYPROJECT_SHARE_PATH)/updatelinks.sh
+	@echo done.
+	@echo
+
+execproxy: TRUEPATH_INSTALL = $(shell echo $(PATH_INSTALL) | sed -e 's/debian\/$(progname)//g')
+execproxy: execproxy.c
+	gcc execproxy.c -DMODULE_PATH=\"$(TRUEPATH_INSTALL)/wrapper.pyo\" -o _$(progname)
+	strip _$(progname)
+
+### Extendable targets
+
+# target: help
+define help/body
+	@echo '=== Configuration variables:'
+	@echo 'INSTALL_SUID   # if not empty string, install program suid'
+	@echo 'INSTALL_NODOC  # if not empty string, compile without docstrings'
+	@echo
+
+	@echo '=== Targets:'
+	@echo 'install   [ prefix=path/to/usr ] # default: prefix=$(value prefix)'
+	@echo 'uninstall [ prefix=path/to/usr ]'
+	@echo
+	@echo 'updatelinks                      # update toolkit wrapper links'
+	@echo
+	@echo 'rename name=<newname>'
+	@echo 'clean'
+	@echo
+	@echo 'dist                             # create distribution tarball'
+	@echo 'gitdist                          # create git distribution tarball'
+endef
+
+# target: build
+build/deps ?= execproxy
+define build/body
+	$(PYCC) pylib/*.py *.py
+endef
+
+# target: install
+install/deps ?= build
+define install/body
+	@echo
+	@echo \*\* CONFIG: prefix = $(prefix) \*\*
+	@echo 
+
+	install -d $(PATH_BIN) $(PATH_INSTALL) $(PATH_INSTALL_LIB) $(PATH_INSTALL_LIBEXEC)
+
+	# if contrib exists
+	contrib=$(wildcard contrib/*); \
+	if [ "$$contrib" ]; then \
+		mkdir -p $(PATH_INSTALL_CONTRIB); \
+		cp -a contrib/* $(PATH_INSTALL_CONTRIB); \
+	fi
+
+	install -m 644 pylib/*.pyo $(PATH_INSTALL_LIB)
+	-install -m 755 libexec/* $(PATH_INSTALL_LIBEXEC)
+
+	install -m 644 wrapper.pyo $(PATH_INSTALL)
+	python -O wrapper.py --version > $(PATH_INSTALL)/version.txt
+
+	for f in $(progname)*; do \
+		if [ -x $$f ]; then \
+			cp -P $$f $(PATH_BIN); \
+		fi; \
+	done
+	rm -f $(PATH_BIN)/$(progname)
+	install -m $(INSTALL_FILE_MOD) _$(progname) $(PATH_BIN)/$(progname)
+endef
+
+# target: uninstall
+define uninstall/body
+	rm -rf $(PATH_INSTALL)
+	rm -rf $(PATH_INSTALL_SHARE)
+	rm -f $(PATH_BIN)/$(progname)
+
+	# delete links from PATH_BIN
+	for f in $(progname)-*; do rm -f $(PATH_BIN)/$$f; done
+endef
+
+# target: clean
+define clean/body
+	rm -f pylib/*.pyc pylib/*.pyo *.pyc *.pyo _$(progname)
+endef
+
+# construct target rules
+define extendable_target
+$1: $$($1/deps) $$($1/deps/extra)
+	$$($1/pre)
+	$$($1/body)
+	$$($1/post)
+endef
+
+EXTENDABLE_TARGETS = help build install uninstall clean
+$(foreach target,$(EXTENDABLE_TARGETS),$(eval $(call extendable_target,$(target))))
+
+.PHONY: gitdist dist updatelinks rename debug $(EXTENDABLE_TARGETS)

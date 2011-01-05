c256e0c2ec18d8e160a80a427e09e25e89280fd9
Liraz Siri <liraz@turnkeylinux.org>
2011-01-05 15:11:16 +0200

    centralized debian/rules to share/pyproject/debian-rules.mk

---
+++ share/debian-rules.mk
@@ -0,0 +1,33 @@
+#! /usr/bin/make -f
+
+# Uncomment this to turn on verbose mode.
+#export DH_VERBOSE=1
+
+progname=$(shell awk '/^Source/ {print $$2}' debian/control)
+prefix=debian/$(progname)/usr
+
+build:
+	mkdir -p $(prefix)
+
+clean:
+	$(MAKE) clean
+	dh_clean
+
+install: build
+	dh_clean -k
+	if [ -d docs ]; then dh_installdocs docs/; fi
+	$(MAKE) install prefix=$(prefix)
+	#$(MAKE) install-nodoc prefix=$(prefix)
+
+binary-indep: build install
+
+binary-arch: build install
+	dh_testdir
+	dh_testroot
+	dh_installdeb -a
+	dh_gencontrol -a
+	dh_md5sums -a
+	dh_builddeb -a
+
+binary: binary-indep binary-arch
+.PHONY: build clean binary-indep binary-arch binary install

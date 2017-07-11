b210799cbc6abbc1908de7a59ab5f3730be29450
Chris Lamb <chris@chris-lamb.co.uk>
2017-07-11 10:11:52 +0100

    Support arbitrary CPPFLAGS, CFLAGS & LDFLAGS>

--- share/pyproject.mk
+++ share/pyproject.mk
@@ -68,7 +68,7 @@ updatelinks:
 	@echo
 
 execproxy: execproxy.c
-	$(CC) execproxy.c -DMODULE_PATH=\"$(call truepath,$(PATH_INSTALL))/wrapper.py\" -o _$(progname)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(LDFLAGS) execproxy.c -DMODULE_PATH=\"$(call truepath,$(PATH_INSTALL))/wrapper.py\" -o _$(progname)
 	strip _$(progname)
 
 ### Extendable targets
